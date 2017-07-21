ffcdaf3c8bd022011575077e388e38805ae583d8
Kutafin Oleg <geloksmmm@gmail.com>
2017-07-21 17:21:29 +0300

    2st commit

---
+++ src/main/java/Math.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Kutafin Oleg on 21.07.2017.
+ */
+public class Math {
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    double minus(double a, double b) {
+        return a - b;
+    }
+
+    double multiply(double a, double b) {
+        return a * b;
+    }
+
+    double divide(double a, double b) {
+        return a / b;
+    }
+
+    double power(double a, double b) {
+        return a%b;
+    }
+
+}
--- src/main/java/SImpleCalculator.java
+++ src/main/java/SImpleCalculator.java
@@ -1,5 +1,63 @@
+import java.util.Scanner;
+
 /**
  * Created by Kutafin Oleg on 21.07.2017.
  */
-public class SImpleCalculator {
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Math Math = new Math();
+        double answer = 0;
+        double firstNumber, secondNumber;
+        char operator;
+        boolean done = false;
+        String exit = "";
+
+        while (!done) {
+            System.out.print("Please enter first number: ");
+            firstNumber = scanner.nextDouble();
+            System.out.println("Enter your operator + - / *");
+            operator = scanner.next().charAt(0);
+            System.out.print("Please enter second number:");
+            secondNumber = scanner.nextDouble();
+
+
+            switch (operator) {
+                case '+':
+                    answer = Math.add(firstNumber, secondNumber);
+                    break;
+                case '-':
+                    answer = Math.minus(firstNumber, secondNumber);
+                    break;
+                case '*':
+                    answer = Math.multiply(firstNumber, secondNumber);
+                    break;
+                case '/':
+                    answer = Math.divide(firstNumber, secondNumber);
+                    break;
+                case '^':
+                    answer = Math.power(firstNumber, secondNumber);
+                    break;
+                case 'n':
+                    System.exit(0);
+            }
+
+            System.out.println(answer);
+            System.out.println("Do you want to exit y/n ?");
+            exit = scanner.next();
+            switch (exit) {
+                case "y":
+                    done = true;
+                    break;
+            }
+        }
+
+        scanner.close();
+
+    }
+
+
 }
+
