2f956ff5211e1d5c0204435f551761fade009f60
Qoopi <geloksmmm@gmail.com>
2017-07-28 19:43:28 +0300

    github to slack test

--- car/src/main/java/car.java
+++ car/src/main/java/car.java
@@ -4,7 +4,7 @@
  */
 public class car {
 
-    // slak tests
+    // slak tests 22
     public static void main (String [] args) {
         for (int i = 0; i < 100; i++) {
             int rand = (int) (Math.random() * 10 + 1);
